70c0db0ce04dbcdc8217cc4e00e05a1497ba08e5
asucarlos <kuwahara.mee@gmail.com>
2019-10-10 18:35:28 -0400

    change port to 8080

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const port = 8000;
+const port = 8080;
 const database = require("./database.js");
 
 //helpers
