66d410582efb9dd16b7b2e1b3322505a048e9248
asucarlos <kuwahara.mee@gmail.com>
2019-10-16 22:56:01 -0400

    change modal with hooks

--- screens/AddListScreen.js
+++ screens/AddListScreen.js
@@ -1,86 +1,78 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Text, View, Alert, StyleSheet } from "react-native";
 import { Button, Form, Item, Input, Textarea, Container, Content } from "native-base";
 import { AntDesign } from "@expo/vector-icons";
 
-class AddListScreen extends Component {
-  state = {
-    modalVisible: false,
-    iconSize: 30
-  };
+AddListScreen = ({ submitHandler }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [iconSize, setIconSize] = useState(30);
 
-  setModalVisible(visible) {
-    this.setState({ modalVisible: visible });
-  }
-
-  render() {
-    return (
-      <View style={{ marginTop: 22 }}>
-        <Modal
-          animationType="slide"
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            Alert.alert("Modal has been closed.");
-          }}
-        >
-          <View style={{ marginTop: 22 }}>
-            <Form
-              model="newList"
-              onSubmit={list => {
-                return console.log(list);
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={{ marginTop: 22 }}>
+          <Form
+            model="newList"
+            onSubmit={list => {
+              return console.log("this is list", list);
+            }}
+          >
+            <Button
+              onPress={() => {
+                setModalVisible(!modalVisible);
               }}
+              style={styles.closeButton}
             >
-              <Button
-                onPress={() => {
-                  this.setModalVisible(!this.state.modalVisible);
-                }}
-                style={styles.closeButton}
-              >
-                <AntDesign name="close" size={this.state.iconSize} />
-              </Button>
-              <Button style={styles.submitButton}>
-                <Text>Submit</Text>
-              </Button>
-              <Text h1 style={styles.formHeader}>
-                Add Your Listing
-              </Text>
-              <Item rounded style={styles.Input}>
-                <Input placeholder="Your Name" />
-              </Item>
-              <Item rounded>
-                <Input placeholder="Contact Number" keyboardType="numeric" />
-              </Item>
-              <Item rounded>
-                <Input placeholder="Where is the dump?" />
-              </Item>
-              <Item rounded style={styles.Input}>
-                <Input placeholder="Soil Type" />
-              </Item>
-              <Item rounded style={styles.Input}>
-                <Input placeholder="Weight In Tons" keyboardType="numeric" />
-              </Item>
-              <Item rounded>
-                <Input placeholder="Enter Price" keyboardType="numeric" />
-              </Item>
-              <Textarea rowSpan={5} bordered placeholder="Additional Notes" />
-            </Form>
-          </View>
-        </Modal>
+              <AntDesign name="close" size={iconSize} />
+            </Button>
+            <Button style={styles.submitButton}>
+              <Text>Submit</Text>
+            </Button>
+            <Text h1 style={styles.formHeader}>
+              Add Your Listing
+            </Text>
+            <Item rounded style={styles.Input}>
+              <Input placeholder="Your Name" />
+            </Item>
+            <Item rounded>
+              <Input placeholder="Contact Number" keyboardType="numeric" />
+            </Item>
+            <Item rounded>
+              <Input placeholder="Where is the dump?" />
+            </Item>
+            <Item rounded style={styles.Input}>
+              <Input placeholder="Soil Type" />
+            </Item>
+            <Item rounded style={styles.Input}>
+              <Input placeholder="Weight In Tons" keyboardType="numeric" />
+            </Item>
+            <Item rounded>
+              <Input placeholder="Enter Price" keyboardType="numeric" />
+            </Item>
+            <Textarea rowSpan={5} bordered placeholder="Additional Notes" />
+          </Form>
+        </View>
+      </Modal>
 
-        <Button
-          round
-          onPress={() => {
-            this.setModalVisible(true);
-          }}
-          style={styles.addListButton}
-        >
-          <AntDesign name="plus" size={this.state.iconSize} style={styles.iconInButton} />
-        </Button>
-      </View>
-    );
-  }
-}
+      <Button
+        round
+        onPress={() => {
+          setModalVisible(true);
+        }}
+        style={styles.addListButton}
+      >
+        <AntDesign name="plus" size={iconSize} style={styles.iconInButton} />
+      </Button>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   closeButton: {
